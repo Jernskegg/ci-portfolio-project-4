3f8d5e4108450f201444926312ec6f9dd97bbba0
Jernskegg <casper.hille@outlook.com>
2021-07-09 04:00:31 +0200

    Feat: Add table management

--- all_dente_jernskegg/settings.py
+++ all_dente_jernskegg/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['all-dente-jernskegg.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['all-dente-jernskegg.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'coudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- alldente/models.py
+++ alldente/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class table(models.Model):
+    table_number = models.IntegerField(default=1, unique=True)
+    table_chairs = models.IntegerField(default=1,)
+
+    def __str__(self):
+        return f'Table ( {self.table_number} ), chairs: {self.table_chairs}'
