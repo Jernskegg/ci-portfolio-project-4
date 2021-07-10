ac9661860a6d23d0d575aee417a6bd5991da4bf2
Jernskegg <casper.hille@outlook.com>
2021-07-10 18:07:27 +0200

    Feat: Add base html template

--- all_dente_jernskegg/urls.py
+++ all_dente_jernskegg/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from restaurant.views import get_base
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_base, name='home'),
 ]
