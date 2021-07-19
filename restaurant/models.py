1c30bc509a08995161b5d5885eaa9e954972563f
Jernskegg <casper.hille@outlook.com>
2021-07-19 20:40:14 +0200

    Build: Change defaults for reservation form

--- restaurant/models.py
+++ restaurant/models.py
@@ -8,7 +8,7 @@ class table(models.Model):
     table_chairs = models.IntegerField(default=1,)
 
     def __str__(self):
-        return f'Table ( {self.table_number} ), chairs: {self.table_chairs}'
+        return f'Table {self.table_number}, 1-{self.table_chairs} persons'
 
 
 class booking(models.Model):
@@ -18,12 +18,12 @@ class booking(models.Model):
         (time(21, 00,), '9 PM'),
         (time(23, 00,), '11 PM'),
     ]
-    booking_email = models.CharField(default="none", max_length=256)
-    table_number = models.IntegerField(default='0')
+    booking_email = models.EmailField(default="Please enter your email please.", max_length=256)
+    table_number = models.ForeignKey(table, blank=False, default=table, on_delete=models.CASCADE)
     booked_for = models.CharField(max_length=30)
     booking_date = models.DateField()
-    booking_time = models.TimeField(choices=times)
-    phone_number = models.IntegerField(default='0000')
+    booking_time = models.TimeField(choices=times, blank=False, default=times)
+    phone_number = models.CharField(default="0123456789", max_length=30, blank=True)
 
     def __str__(self):
         return f'{self.booking_email} has booked {self.table_number}'\
